cb83c9c3f058b0a9440307b29d49ff5ef56f989c
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-25 18:24:37 +0200

    esercizio terminato

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,7 +8,8 @@ use App\Models\Train;
 class TrainController extends Controller
 {
     public function index(){
-        $trains= Train::all();
+       // $trains= Train::all();
+       $trains = Train::where("data_di_partenza", "=", "2024-10-25")->get();
      
         return view("trains", compact("trains"));
     }
