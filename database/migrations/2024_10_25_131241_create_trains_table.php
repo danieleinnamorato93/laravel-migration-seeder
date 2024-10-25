0d077743406f3214a0535f2877ee307035bcb9c8
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-25 15:54:33 +0200

    migration done

--- database/migrations/2024_10_25_131241_create_trains_table.php
+++ database/migrations/2024_10_25_131241_create_trains_table.php
@@ -9,10 +9,33 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+
+     /*Azienda
+Stazione di partenza
+Stazione di arrivo
+Orario di partenza
+Orario di arrivo
+Codice Treno
+Numero Carrozze
+In orario
+Cancellato 
+*/
+
     public function up(): void
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string("azienda",150);
+            $table->string("stazione_di_partenza",250);
+            $table->string("stazione_di_arrivo",250);
+            $table->date("data_di_partenza");
+            $table->time('orario_di_partenza',0)->nullable();
+            $table->string("codice_treno",250)->unique();
+            $table->integer("numero_carrozze")->nullable();
+            $table->boolean("in_orario")->default(true);
+            $table->boolean("cancellato")->default(false);
+            
+            
             $table->timestamps();
         });
     }
