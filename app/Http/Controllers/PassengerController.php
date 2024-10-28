983cd7bb493b264d99d9f09a9ba8738e615dfe70
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-28 16:25:04 +0100

    PassengerSeeder e dati stampati in pagina

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Passenger;
+use Illuminate\Http\Request;
+
 
 class PassengerController extends Controller
 {
     public function index(){
-    $passengers= Passenger::all();
+    $passengers = Passenger::all();
     return view('passengers.index' , compact("passengers"));
     }
 }
--- database/seeders/PassengerSeeder.php
+++ database/seeders/PassengerSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\Passenger;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
+
+/*$table->id();
+            $table->string("first_name",150)->nullable(false);
+            $table->string("last_name",150)->nullable(false);
+            $table->date("date_of_birth")->nullable(false);
+            $table->boolean("member_card")->default(false);
+            $table->timestamps();
+        });*/
 class PassengerSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     * 
+     * 
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+           for ($i=0; $i < 50 ; $i++) { 
+            $newPassenger = new Passenger();
+            $newPassenger->first_name = $faker->firstName();
+            $newPassenger->last_name = $faker->lastName();
+            $newPassenger->date_of_birth = $faker->dateTimeBetween("-99 years","-16 years");
+            $newPassenger->member_card = $faker->boolean();
+            $newPassenger->save();
+           }
+        }
     }
-}
+
