d1f1b1f38452c972575c892d4b0a0e03076e92a1
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-10-28 17:49:04 +0100

    esercizio_terminato

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -3,16 +3,25 @@
 @section("page-title")
 
 @section("main-content")
-<h1>Passengers</h1>
+<h1>Tickets:</h1>
+{{--   $table->decimal("price",4,2);
+            $table->string("seat_number");
+            $table->dateTime("departure_time");
+            $table->dateTime("arrival_time");
+            $table->timestamps();
+--}}
 
 <div class="row justify-content-center">
-    @foreach ($passengers as $passenger )
+    @foreach ($tickets as $ticket )
 
     <div class="card col-2  m-3">
         <div class="card-body">
-          <h5 class="card-title">Nome Passeggero: {{$passenger->first_name}}</h5>
-          <h5 class="card-title">Cognome Passeggero: {{$passenger->last_name}}</h5>
-          <h5 class="card-title">Data di nascita: {{$passenger->date_of_birth}}</h5>
+          <h5 class="card-title">Prezzo: {{$ticket->price}}</h5>
+          <h5 class="card-title">Posto numero: {{$ticket->seat_number}}</h5>
+          <h5 class="card-title">Partenza: {{$ticket->departure_time}}</h5>
+          <h5 class="card-title">Arrivo: {{$ticket->arrival_time}}</h5>
+       
+         
         </div>
       </div>
       @endforeach
